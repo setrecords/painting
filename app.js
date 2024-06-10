2ce3486cf9d3c8eb4a16b03a6bde544143edbaea
setrecords <otadtoko@gmail.com>
2024-06-10 22:51:47 +0900

    Mouse Painting

--- app.js
+++ app.js
@@ -2,26 +2,48 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
-
 ctx.lineWidth = 2;
 
-const colors = [
-    "#ff3838",
-    "#ffb8b8",
-    "#c56cf0",
-    "#ff9f1a",
-    "#fff200",
-    "#32ff7e",
-    "#7efff5",
-];
-
-function onClick(event) {
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    const color = colors[Math.floor(Math.random() * colors.length)];
-    ctx.strokeStyle = color;
-    ctx.lineTo(event.offsetX, event.offsetY);
-    ctx.stroke();
+// Mouse Painting
+let isPainting = false;
+function onMove(event) {
+    if(isPainting) {
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+        return;
+    }
+    ctx.moveTo(event.offsetX, event.offsetY);
+}
+function onMoveDown(event) {
+    isPainting = true;
+}
+function onMoveUp(event) {
+    isPainting = false;
 }
 
-canvas.addEventListener("mousemove", onClick);
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", onMoveDown);
+canvas.addEventListener("mouseup", onMoveUp);
+canvas.addEventListener("mouseleave", onMoveUp);
+
+// Painting Lines
+// const colors = [
+//     "#ff3838",
+//     "#ffb8b8",
+//     "#c56cf0",
+//     "#ff9f1a",
+//     "#fff200",
+//     "#32ff7e",
+//     "#7efff5",
+// ];
+
+// function onClick(event) {
+//     ctx.beginPath();
+//     ctx.moveTo(0, 0);
+//     const color = colors[Math.floor(Math.random() * colors.length)];
+//     ctx.strokeStyle = color;
+//     ctx.lineTo(event.offsetX, event.offsetY);
+//     ctx.stroke();
+// }
+
+// canvas.addEventListener("mousemove", onClick);
